e4be152a3abf3583d960f2e21380a7868db01374
Luigi Lake <lfdlake@gmail.com>
2017-12-02 12:05:54 -0500

    refactor category filter

--- src/Javascript/CategoryFilter.js
+++ src/Javascript/CategoryFilter.js
@@ -19,104 +19,54 @@ export function categoryFilter(solutions, favorites, category){
   let newSolutions;
   switch(category){
     case "All Solutions":
-      data = solutions.map((solution) => {
-        let favorited = false;
-        if(favorites.includes(solution.id)){
-          favorited = true;
-        }
-        return(
-          {
-            key: solution.id,
-            name: solution.name,
-            contact: solution["#contact"]["name"],
-            image: `https://images.techxlab.org/${solution.image}`,
-            favorited: favorited,
-          }
-        );
-      })
+      newSolutions = solutions;
       break;
     case "Favorites":
       newSolutions = solutions.filter( solution => {
         return(favorites.includes(solution.id))
-      })
-      data = newSolutions.map((solution) => {
-        let favorited = false;
-        if(favorites.includes(solution.id)){
-          favorited = true;
-        }
-        return(
-          {
-            key: solution.id,
-            name: solution.name,
-            contact: solution["#contact"]["name"],
-            image: `https://images.techxlab.org/${solution.image}`,
-            favorited: favorited,
-          }
-        );
       })
       break;
     case "Agriculture & Tools":
       newSolutions = solutions.filter( solution => {
         return(solution.category.includes("agriculture"))
       })
-      data = newSolutions.map((solution) => {
-        return(
-          renderSolution(solution,favorites)
-        );
-      })
       break;
     case "Energy & Cooking":
       newSolutions = solutions.filter( solution => {
         return(solution.category.includes("energy"))
-      })
-      data = newSolutions.map((solution) => {
-        return(
-          renderSolution(solution,favorites)
-        );
       })
       break;
     case "Health & Medical":
       newSolutions = solutions.filter( solution => {
         return(solution.category.includes("medical"))
       })
-      data = newSolutions.map((solution) => {
-        return(
-          renderSolution(solution,favorites)
-        );
-      })
       break;
     case "Education & Connectivity":
       newSolutions = solutions.filter( solution => {
         return(solution.category.includes("education"))
-      })
-      data = newSolutions.map((solution) => {
-        return(
-          renderSolution(solution,favorites)
-        );
       })
       break;
     case "Housing & Transport":
       newSolutions = solutions.filter( solution => {
         return(solution.category.includes("housing"))
       })
-      data = newSolutions.map((solution) => {
-        return(
-          renderSolution(solution,favorites)
-        );
-      })
       break;
     case "Water & Sanitation":
       newSolutions = solutions.filter( solution => {
         return(solution.category.includes("water"))
       })
-      data = newSolutions.map((solution) => {
-        return(
-          renderSolution(solution,favorites)
-        );
+      break;
+    case "Additional Solutions":
+      newSolutions = solutions.filter( solution => {
+        return(solution.category.includes("other"))
       })
       break;
-
   }
+  data = newSolutions.map((solution) => {
+    return(
+      renderSolution(solution, favorites)
+    )
+  })
   return( data )
 }
 
