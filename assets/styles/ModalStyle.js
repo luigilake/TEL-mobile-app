57ac3e8a4392ccea03d24f4d4efdba268bed749b
Luigi Lake <lfdlake@gmail.com>
2017-12-04 22:52:25 -0500

    add image sizes

--- assets/styles/ModalStyle.js
+++ assets/styles/ModalStyle.js
@@ -11,10 +11,18 @@ const styles = StyleSheet.create({
   closeIcon: {
     height: 40,
     width: 40,
-    margin: 10,
+    margin: 5,
   },
   closeView: {
     alignItems: 'flex-end',
+    position: 'absolute',
+    marginTop: 22,
+  },
+  mainImage: {
+    width: 400,
+    height: 400,
+    minHeight: 200,
+    backgroundColor: '#333333',
   }
 });
 
--- src/containers/SolutionModal.js
+++ src/containers/SolutionModal.js
@@ -6,6 +6,8 @@ import styles from '../../assets/styles/ModalStyle'
 export default class SolutionModal extends React.Component {
   render(){
     let data = this.props.data
+    let image = `https://images.techxlab.org/${data.image}`
+
     return (
         <Modal
           visible={this.props.modalOpen}
@@ -13,16 +15,17 @@ export default class SolutionModal extends React.Component {
           style={styles.container}
         >
           <View style={styles.topBar}></View>
-          <TouchableOpacity onPress={this.props.closeModal}>
-            <View style={styles.closeView}>
-              <Image style={styles.closeIcon} source={require('../../assets/images/close.png')}/>
-            </View>
             <View>
+              <Image resizeMode='contain' style={styles.mainImage} source={{uri: image}} />
               <Text>
                 {data.name}
               </Text>
             </View>
-          </TouchableOpacity>
+            <View style={styles.closeView}>
+              <TouchableOpacity onPress={this.props.closeModal}>
+              <Image style={styles.closeIcon} source={require('../../assets/images/close.png')}/>
+              </TouchableOpacity>
+            </View>
         </Modal>
     );
   }
