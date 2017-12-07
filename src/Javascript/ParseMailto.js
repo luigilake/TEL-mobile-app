dd0d4f27a7f016dc69c0d37dc731f4f02df599e3
Luigi Lake <lfdlake@gmail.com>
2017-12-07 15:45:55 -0500

    add regex for mailto parser, set default mailto if none

--- src/Javascript/ParseMailto.js
+++ src/Javascript/ParseMailto.js
@@ -1,13 +1,19 @@
 export function parseMailto(data){
+  let mailto;
   if(data["#contact"]["_txt"].includes('mailto')){
-    return(
-      'hello world'
-    )
+    let results = data["#contact"]["_txt"].match(/\((.*?)(?=\))/g);
+    let mailtoResult = results.find( result => result.includes('mailto'));
+    if(mailtoResult[0] == '('){
+      mailtoResult = mailtoResult.slice(1)
+    }
+    mailto = mailtoResult
   } else {
-    return(
-      `mailto: notifications@techxlab.org?subject=Requesting Information on ${data["name"]} from ${data["#contact"]["name"]}`
-    )
+    mailto = `mailto:notifications@techxlab.org?subject=Requesting Information on ${data["name"]} from ${data["#contact"]["name"]}`
   }
+  console.log(mailto)
+  return(
+    mailto
+  );
 }
 
 export default parseMailto;
