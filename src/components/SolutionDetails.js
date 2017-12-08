905d2b5e2d60047c87afc41363e250040390c12d
Luigi Lake <lfdlake@gmail.com>
2017-12-07 19:46:51 -0500

    remove unneccessary imports

--- src/components/SolutionDetails.js
+++ src/components/SolutionDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Linking, Animated, ScrollView, Modal, View, Text, Image, TouchableOpacity } from 'react-native';
+import { Animated, ScrollView, View, Text } from 'react-native';
 
 import styles from '../../assets/styles/ModalStyle'
 import ModalText from './ModalText'
