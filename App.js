8d1fbedbb00055bd413eb7fcf732741c2a2a8519
Luigi Lake <lfdlake@gmail.com>
2017-12-18 14:44:44 -0500

    add provider to app.js

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 import Main from './src/Main'
+import store from './src/store'
 
 const App = props => {
   return (
-    <Main />
+    <Provider store={store}>
+      <Main />
+    </Provider>
   );
 }
 
